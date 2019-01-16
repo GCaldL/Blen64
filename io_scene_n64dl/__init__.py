acdbf7990eec58d7aee0dc977d98eb18c0604d3c
astronotter <ottersocks@outlook.com>
2019-01-16 10:46:19 -0500

    Bug fixes: Reorder triangulation. Set vertex alpha to 255.

--- io_scene_n64dl/__init__.py
+++ io_scene_n64dl/__init__.py
@@ -19,38 +19,9 @@ import os
 import random
 import string
 
-def descends(parents, child):
-  while child and child not in parents:
-    child = child.parent
-  return child != None
-
 class ExportDL(bpy.types.Operator):
   bl_idname = "export.n64_dl"
   bl_label = "Export N64 DL"
-  
-  scale = bpy.props.FloatProperty(
-    name        = "Scale",
-    description = "Scalar multiplier.",
-    default     = 100.0,
-    min         = 0.01,
-    max         = 1000.0,
-    precision   = 2)
-  twidth = bpy.props.FloatProperty(
-    name        = "Texture Width",
-    description = "Texture width in texels.",
-    default     = 32.0,
-    min         = 1.0,
-    max         = 1024.0,
-    precision   = 1
-  )
-  theight = bpy.props.FloatProperty(
-    name        = "Texture Height",
-    description = "Texture height in texels.",
-    default     = 32.0,
-    min         = 1.0,
-    max         = 1024.0,
-    precision   = 1
-  )
   
   filepath = bpy.props.StringProperty(subtype="FILE_PATH")
   
@@ -75,22 +46,15 @@ class ExportDL(bpy.types.Operator):
     return name
 
   def export(self, o, obj):
+    s =  100     # Scale constant
     bitshift = 6 # Bitshift mod
+    tsize = 32   # Texture size in pixels
     loadlim = 30 # Ammount of verts the sytem will load at a time 32 max limit
-    
-    trans = bpy.context.object.matrix_world
     
     name = self.clean_name(obj.name)
     vert = obj.data.vertices
     poly = obj.data.polygons
     uv = obj.data.uv_layers.active
-
-    #Info
-    o.write("/*\nModel: %s \n" % name)
-    o.write("Verts: %s \n" % len(vert))
-    o.write("Tris: %s \n" % len(poly))
-    o.write("Exported from blender using blen64.\n")
-    o.write("github.com/GCaldL/Blen64/\n*/\n")
 
     ###Triangulate
     #Check for polys w/ more than 3 verts
@@ -100,20 +64,22 @@ class ExportDL(bpy.types.Operator):
             bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
             bpy.ops.object.editmode_toggle()
             break
+    
+    #Info
+    o.write("/*\nModel: %s \n" % name)
+    o.write("Verts: %s \n" % len(vert))
+    o.write("Tris: %s \n" % len(poly))
+    o.write("Exported from blender using blen64.\n")
+    o.write("github.com/GCaldL/Blen64/\n*/\n")
 
     #Vertex List
     o.write("Vtx %s_VertList[] = {\n" % name)
     for face in poly:
         for vert, loop in zip(face.vertices, face.loop_indices):
-            coord = obj.data.vertices[vert].co * obj.matrix_world
+            coord = obj.data.vertices[vert].co
             uv = obj.data.uv_layers.active.data[loop].uv if obj.data.uv_layers.active else (0,0)
             vcol = obj.data.vertex_colors.active.data[loop].color if obj.data.vertex_colors.active else (1,1,1,1)
-            o.write("   { %.2f, %.2f, %.2f, %i, %i << 6, %i << 6, %i, %i, %i, %i},\n"
-              % (coord.x*self.scale, coord.y*self.scale, coord.z*self.scale,
-                 random.randint(0,255),
-                 uv[0]*self.twidth, (1-uv[1])*self.theight,
-                 vcol[0]*255, vcol[1]*255, vcol[2]*255,
-                 random.randint(0,255)))
+            o.write("   { %.2f, %.2f, %.2f, %i, %i << 6, %i << 6, %i, %i, %i, %i},\n" % (coord.x*s, coord.y*s, coord.z*s, random.randint(0,255), uv[0]*tsize, (1-uv[1])*tsize, vcol[0]*255, vcol[1]*255, vcol[2]*255,random.randint(0,255)))
     o.write("};\n\n")
 
     #Face List
@@ -134,9 +100,8 @@ class ExportDL(bpy.types.Operator):
 
   def execute(self, context):
     file = open(self.filepath, 'w')
-    for obj in bpy.context.scene.objects:
-      if obj.type == 'MESH' and descends(bpy.context.selected_objects, obj):
-        self.export(file, obj)
+    for obj in bpy.context.selected_objects:
+      self.export(file, obj)
     return {'FINISHED'}
     
   def invoke(self, context, event):
@@ -150,6 +115,7 @@ def menu_func(self, context):
 def register():
   bpy.utils.register_class(ExportDL)
   bpy.types.INFO_MT_file_export.append(menu_func)
+  print("TEST")
   
 def unregister():
   bpy.types.INFO_MT_file_export.remove(menu_func)
